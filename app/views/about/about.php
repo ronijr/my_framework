75a1bd4fbc6658c3c8476492e47fd148fbc3ec17
ronijr <ronijakarianto01@gmail.com>
2017-02-01 23:44:57 +0700

    tambah about file.php

---
+++ app/views/about/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html leng="en">
+  <head>
+      <title>Test Input</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="<?php echo $data['base_url']; ?>">
+  </head>
+<body>
+<div class="container">
+  <h1>Hello</h1>
+</div>
+</body>
+</html>
