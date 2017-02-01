28d8eb2856217e5fa8165616f55eb692dfbce5ab
ronijr <ronijakarianto01@gmail.com>
2017-02-01 23:41:52 +0700

    commit-massage "Edit file"

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,4 +5,13 @@ class About extends Controller{
   public function index(){
     echo "ok";
   }
+
+  public function TestInput(){
+    $app = new App();
+    $data = array(
+
+      'base_url' => $app->base_url(),
+    );
+    $this->view('about/about', $data);
+  }
 }
